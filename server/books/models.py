8b6584ae3eff60f256dfe7baa4d88953d7cc04e2
SridharMurali1995 <sridhar.murali@happyfox.com>
2021-02-17 12:00:14 +0530

    Notes Basic MVP backend changes

--- server/books/models.py
+++ server/books/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=500)
     author = models.CharField(max_length=100)
+    topic = models.CharField(max_length=500)
     description = models.TextField()
+    likes = models.BigIntegerField(default=0)
